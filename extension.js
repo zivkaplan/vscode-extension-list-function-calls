b59dfacb0f93a133d0036ea9e5cdd3bb3ee6329c
ziv.kaplan <zivkaplan@gmail.com>
2022-02-01 22:21:48 +0200

    output. Regex not perfect

--- extension.js
+++ extension.js
@@ -8,34 +8,52 @@ const vscode = require('vscode');
 /**
  * @param {vscode.ExtensionContext} context
  */
+
 function activate(context) {
 
     let activeEditor = vscode.window.activeTextEditor;
+    
     function GetList()
     {
         if (!activeEditor) 
         {
+            vscode.window.showInformationMessage('No active editor');
 			return;
         }
 
-        const regEx = '(.*?)()';
+        const matches = [];    
 		const text = activeEditor.document.getText();
-        
+        const regEx =  /\b.\S+\(.*\)/g;
+        // [^intvoidcharlongfunctionprivepublicdoublefloatdef_t\s]\s\b\S.\S+\(.*\)
+        text.split('\n').forEach((line, num) => {
+            [...line.matchAll(regEx)].forEach(match =>
+                {
+                    matches.push({line: num, result: match});                    
+                });
+        });
+
+        return matches;
+    }
+        /* 
         vscode.window.showTextDocument(document, {
             viewColumn: vscode.ViewColumn.Beside
-        });
-        
-        activeEditor.GetList( smallNumbers);
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "list-function-calls" is now active!');
+        }); */
+
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('list-function-calls.helloWorld', function () {
+	let disposable = vscode.commands.registerCommand('list-function-calls.GetList', function () {
 		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
+    const outputChannel = vscode.window.createOutputChannel("List-Function-Calls");
+    GetList().forEach(element => {       
+        outputChannel.appendLine(`-line ${element.line}: ${element.result}`);
+    });
+    outputChannel.show();
+    console.log(GetList()[0])
+    /* 
+    outputChannel.sendText(GetList().join("\n")); 
+    outputChannel.show();
+	 */	// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from List Function Calls!');
 	});
 
